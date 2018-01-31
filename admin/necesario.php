51ee88643d71e42f1c3c29e9fe1eecc981937df8
lxlDanilxl <lxlDanilxlpro@gmail,com>
2018-01-31 19:30:44 +0100

    .

--- admin/necesario.php
+++ admin/necesario.php
@@ -37,17 +37,30 @@
 		$type=$result->fetchColumn();
 		return $type;
 	}
-	function selecttype() {
+	function selecttype($id) {
 		GLOBAL $conn;
 		$qry="SELECT * FROM type_games";
 		$result=$conn->prepare($qry);
 		$result->execute();
 
-		echo "<select name='type'>";
-		while ($types = $result->fetch()) {
-			echo "<option value='".$types[0]."'>".$types[1]."</option>";
+		$select="<select form='edit' name='a4'>";
+		if (!isset($id)) {
+			$id=1;
 		}
-		echo "</select>";
+		while ($types = $result->fetch()) {
+			if (isset($id)) {
+				if (gettypes($id)==$types[0]) {
+					$select.="<option selected value='".$types[0]."'>".$types[1]."</option>";
+				} else {
+					$select.="<option value='".$types[0]."'>".$types[1]."</option>";
+				}
+			} else {
+				$select.="<option value='".$types[0]."'>".$types[1]."</option>";
+			}
+		}
+		$select.="</select>";
+		return $select;
+		unset($select);
 	}
 		function getclasses($id) {
 		GLOBAL $conn;
@@ -58,17 +71,27 @@
 		$class=$result->fetchColumn();
 		return $class;
 	}
-	function selectclass() {
+	function selectclass($id) {
 		GLOBAL $conn;
 		$qry="SELECT * FROM class_games";
 		$result=$conn->prepare($qry);
 		$result->execute();
 
-		echo "<select name='class'>";
+		$select="<select form='edit' name='a5'>";
 		while ($classes = $result->fetch()) {
-			echo "<option value='".$classes[0]."'>".$classes[1]."</option>";
+			if (isset($id)) {
+				if (getclasses($id)==$classes[0]) {
+					$select.="<option selected value='".$classes[0]."'>".$classes[1]."</option>";
+				} else {
+					$select.="<option value='".$classes[0]."'>".$classes[1]."</option>";
+				}
+			} else {
+				$select.="<option value='".$classes[0]."'>".$classes[1]."</option>";
+			}
 		}
-		echo "</select>";
+		$select.="</select>";
+		return $select;
+		unset($select);
 	}
 	function listgames() {
 		if (!no_permission(2)) {
@@ -116,7 +139,7 @@
 	}
 	function confirmdel() {
 		if (!no_permission(2)) {
-			echo "<form method='post' action='admin.php?f=delgame'>
+			echo "<form method='post' action='admin.php?f=delgame'><script>alert('Estás a punto de eliminar el evento número ".$_COOKIE['evento']."!')</script>
 			<button type='submit' formmethod='post' class='btn btn-primary' name='confirm' value='del'>Confirmar</button>";
 		}
 	}
@@ -126,7 +149,6 @@
 				confirmdel();
 			} elseif ($_POST['listgame']==='edit') {
 				editgame();
-				header("Refresh:0; url=http://localhost/intranet/admin.php?f=ver_juego");
 			}
 	}
 	function editgame() {
@@ -137,43 +159,59 @@
 			$result->bindParam(':id', $_POST['evento']);
 			$result->execute();
 			$id=$result->fetch();
-			echo "<table border='1'><tr><td>ID</td><td>Título</td><td>Descripción</td><td>Fecha</td><td>Día</td><td>Hora</td><td>Máx</td><td>Equipo mínimo</td><td>Equipo máximo</td><td>Fin registro</td></tr>";
-			echo "<form method='post' action='http://localhost/intranet/admin.php?f=savegame'><tr><td><input type='text' name='id' readonly='readonly' value='".($id[0]);
+			echo 
+				"<table border='1'>
+					<tr>
+						<td>ID</td>
+						<td>Título</td>
+						<td>Descripción</td>
+						<td>Imagen</td>
+						<td>Tipo</td>
+						<td>Clase</td>
+						<td>Equipo mínimo</td>
+						<td>Equipo máximo</td>
+						<td>Fecha</td>
+						<td>Fin registro</td>
+					</tr>";
+			echo "<form method='post' id='edit' action='http://localhost/intranet/admin.php?f=savegame'><tr><td><input type='text' name='id' readonly='readonly' value='".($id[0]);
 			echo "'></td>
-			<td><input type='text' name='a1' value='".utf8_encode($id[2])."'></td>
-			<td><input type='text' name='a2' value='".utf8_encode($id[3])."'></td>
-			<td><input type='text' name='a3' value='".$id[4]."'  placeholder='aaaa-mm-dd hh:mm:ss'></td>
-			<td><input type='text' name='a4' value='".utf8_encode($id[5])."'></td>
-			<td><input type='time' name='a5' value='".$id[6]."'></td>
-			<td><input type='text' name='a6' value='".$id[7]."'></td>
-			<td><input type='text' name='a7' value='".$id[8]."'></td>
-			<td><input type='text' name='a8' value='".$id[9]."'></td>
-			<td><input type='text' name='a9' value='".$id[10]."' placeholder='aaaa-mm-dd hh:mm:ss'></td></tr>
-			<tr><td><button type='submit' formmethod='post' class='btn btn-primary'>Guardar</button></td></tr></table>";
-			print_r($id);
+			<script>alert('Es muy importante que no la pifies en el formato de las fechas! (AAAA-MM-DD HH:MM:SS)')</script>
+			<td><input type='text' name='a1' value='".utf8_encode($id[1])."'></td>
+			<td><input type='text' name='a2' value='".utf8_encode($id[2])."'></td>
+			<td><input type='text' name='a3' value='".$id[3]."'></td>
+			<td>".selecttype($id[4])."</td>
+			<td>".selectclass($id[5])."</td>
+			<td><input type='text' name='a6' value='".$id[6]."'></td>
+			<td><input type='text' name='a7' value='".$id[7]."'></td>
+			<td><input type='text' name='a8' placeholder='AAAA-MM-DD HH:MM:SS' value='".$id[8]."'></td>
+			<td><input type='text' name='a9' placeholder='AAAA-MM-DD HH:MM:SS' value='".$id[9]."'></td>
+			</table>
+			<br>
+			<button type='submit' formmethod='post' class='btn btn-primary'>Guardar</button>";
+
 		}
 	}
 	function savegame() {
 		if (!no_permission(2)) {
 			if(isset($_GET['f']) AND $_GET['f']=='savegame') {
 				GLOBAL $conn;
-				$qry="UPDATE games SET `title`=:a1 , `description`=:a2, `day_week`=:a4, `hour`=:a5, `max`=:a6, `min_team`=:a7, `max_team`=:a8, `date`=:a3, `date_end_reg`=:a9 WHERE `id`=:id";
+				$qry="UPDATE games SET `name`=:a1 , `description`=:a2, `image`=:a3, `type`=:a4, `class`=:a5, `max`=:a6, `min`=:a7, `date_event`=:a8, `date_max`=:a9 WHERE `id`=:id";
 				$result=$conn->prepare($qry);
 				$a1=utf8_decode($_POST['a1']);
 				$a2=utf8_decode($_POST['a2']);
-				$a4=utf8_decode($_POST['a4']);
-				$result->bindParam(':id',$_POST['id']);
-				$result->bindParam(':a1',$a1);
-				$result->bindParam(':a2',$a2);
-				$result->bindParam(':a3',$_POST['a3']);
-				$result->bindParam(':a4',$a4);
-				$result->bindParam(':a5',$_POST['a5']);
-				$result->bindParam(':a6',$_POST['a6']);
-				$result->bindParam(':a7',$_POST['a7']);
-				$result->bindParam(':a8',$_POST['a8']);
-				$result->bindParam(':a9',$_POST['a9']);
+				$result->bindParam(':id', $_POST['id']);
+				$result->bindParam(':a1', $a1);
+				$result->bindParam(':a2', $a2);
+				$result->bindParam(':a3', $_POST['a3']);
+				$result->bindParam(':a4', $_POST['a4']);
+				$result->bindParam(':a5', $_POST['a5']);
+				$result->bindParam(':a6', $_POST['a6']);
+				$result->bindParam(':a7', $_POST['a7']);
+				$result->bindParam(':a8', $_POST['a8']);
+				$result->bindParam(':a9', $_POST['a9']);
 				$result->execute();
 			}
+			header("Refresh:0; url=http://localhost/intranet/admin.php?f=ver_juego");
 		}
 	}
 	function creategame() {
@@ -183,35 +221,50 @@
 			$result=$conn->prepare($qry);
 			$result->execute();
 			$id=$result->fetchColumn();
-			echo "<table width='1000 border='1'><tr><td>ID</td><td>Título</td><td width='20%'>Descripción</td><td>Fecha</td><td>Día</td><td>Hora</td><td>Máx</td><td>Equipo mínimo</td><td>Equipo máximo</td><td>Fin registro</td></tr>";
-			echo "<form method='post' action='http://localhost/intranet/admin.php?f=newgame'><tr><td><input type='text' name='id' readonly='readonly' value='".($id+1);
+			echo 
+				"<table border='1'>
+					<tr>
+						<td>ID</td>
+						<td>Título</td>
+						<td>Descripción</td>
+						<td>Imagen</td>
+						<td>Tipo</td>
+						<td>Clase</td>
+						<td>Equipo mínimo</td>
+						<td>Equipo máximo</td>
+						<td>Fecha</td>
+						<td>Fin registro</td>
+					</tr>";
+			echo "<form method='post' id='edit' action='admin.php?f=newgame'><tr><td><input type='text' name='id' readonly='readonly' value='".($id+1);
 			echo "'></td>
+			<script>alert('Es muy importante que no la pifies en el formato de las fechas! (AAAA-MM-DD HH:MM:SS)')</script>
 			<td><input type='text' name='a1'></td>
 			<td><input type='text' name='a2'></td>
-			<td><input type='text' name='a3' placeholder='aaaa-mm-dd hh:mm:ss'></td>
-			<td><input type='text' name='a4'></td>
-			<td><input type='time' name='a5'></td>
+			<td><input type='text' name='a3'></td>
+			<td>".selecttype(1)."</td>
+			<td>".selectclass(1)."</td>
 			<td><input type='text' name='a6'></td>
 			<td><input type='text' name='a7'></td>
-			<td><input type='text' name='a8'></td>
-			<td><input type='text' name='a9' placeholder='aaaa-mm-dd hh:mm:ss'></td></tr>
-			<tr><td><button type='submit' formmethod='post' class='btn btn-primary'>Crear</button></td></tr></table>";
+			<td><input type='text' name='a8' placeholder='AAAA-MM-DD HH:MM:SS'></td>
+			<td><input type='text' name='a9' placeholder='AAAA-MM-DD HH:MM:SS'></td>
+			</table>
+			<br>
+			<button type='submit' formmethod='post' class='btn btn-primary'>Crear</button>";
 		}
 	}
 	function newgame() {
 		if(!no_permission(2)) {
 			if(isset($_GET['f']) AND $_GET['f']=='newgame') {
 				GLOBAL $conn;
-				$qry="INSERT INTO games (`id`, `title`, `description`, `day_week`, `hour`, `max`, `min_team`, `max_team`, `date`, `date_end_reg`) VALUES (:id, :a1, :a2, :a4, :a5, :a6, :a7, :a8, :a3, :a9)";
+				$qry="INSERT INTO games (`id`, `name`, `description`, `image`, `type`, `class`, `max`, `min`, `date_event`, `date_max`) VALUES (:id, :a1, :a2, :a3, :a4, :a5, :a6, :a7, :a8, :a9)";
 				$result=$conn->prepare($qry);
 				$a1=utf8_decode($_POST['a1']);
 				$a2=utf8_decode($_POST['a2']);
-				$a4=utf8_decode($_POST['a4']);
 				$result->bindParam(':id',$_POST['id']);
 				$result->bindParam(':a1',$a1);
 				$result->bindParam(':a2',$a2);
 				$result->bindParam(':a3',$_POST['a3']);
-				$result->bindParam(':a4',$a4);
+				$result->bindParam(':a4',$_POST['a4']);
 				$result->bindParam(':a5',$_POST['a5']);
 				$result->bindParam(':a6',$_POST['a6']);
 				$result->bindParam(':a7',$_POST['a7']);
@@ -219,6 +272,7 @@
 				$result->bindParam(':a9',$_POST['a9']);
 				$result->execute();
 			}
+			header("Refresh:0; url=http://localhost/intranet/admin.php?f=ver_juego");
 		}
 	}
 	function delgame() {
